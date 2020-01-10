5e43f7ac70a3558c5e3897c9944c4175c1270028
csch <cschwarzbeck@TECHNIK-CON>
2020-01-10 10:01:36 +0100

    Adding LanguageSettingsTest

---
+++ Test/HelpersTest/LocalizationTest/LanguageSettingsTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+using Sidekick.Helpers.POETradeAPI;
+using Sidekick.Helpers.Localization;
+
+namespace Test.HelpersTest.LocalizationTest
+{
+    [TestClass]
+    public class LanguageSettingsTest
+    {
+        [TestInitialize]
+        public void ReplaceTradeClient()
+        {
+            var tradeClientMock = new Mock<ITradeClient>(MockBehavior.Strict);
+            tradeClientMock.Setup(tradeClient => tradeClient.FetchAPIData()).Returns(Task.CompletedTask);
+            TradeClient.Client = tradeClientMock.Object;
+        }
+
+        // TODO add proper test cases
+        [DataTestMethod]
+        [DataRow("", Language.English)]
+        public void DetectLanguage_WithInput_IsExpected(string input, Language expected)
+        {
+            LanguageSettings.DetectLanguage(input);
+            Assert.AreEqual(expected, LanguageSettings.CurrentLanguage);
+        }
+    }
+}
