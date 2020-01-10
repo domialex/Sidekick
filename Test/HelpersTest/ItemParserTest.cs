09e18c66809ec19a1c2716437f00b7c3486be129
csch <cschwarzbeck@TECHNIK-CON>
2020-01-10 14:43:25 +0100

    preparing ItemParserTest

--- Test/HelpersTest/ItemParserTest.cs
+++ Test/HelpersTest/ItemParserTest.cs
@@ -4,13 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Sidekick.Helpers;
 
 namespace Test.HelpersTest
 {
-    //TODO add tests
     [TestClass]
     public class ItemParserTest
     {
-
+        //TODO add test cases
+        //Item will need to be made IEquatable<Item> for the tests to work
+        [DataTestMethod]
+        [DataRow("", null)]
+        public void ParseItem_WithInput_IsExpected(string input, Item expected)
+        {
+            var result = ItemParser.ParseItem(input);
+            Assert.AreEqual(expected, result);
+        }
     }
 }
