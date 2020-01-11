215e36d0f98e222d663f20319f1eaa14958ce420
Onur Tanrikulu <onurtnrkl@gmail.com>
2020-01-11 17:27:24 +0300

    Add RarityToColorConverter
    * Converters folder added.
    * RarityColorConverter added for converting rarity names to official rarity colors.

---
+++ source/Windows/Overlay/Converters/RarityToColorConverter.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Sidekick.Windows.Overlay.Converters
+{
+    public class RarityToColorConverter : IValueConverter
+    {
+        private Color GetRarityColor(string rarity)
+        {
+            switch (rarity)
+            {
+                case "Normal":
+                    return Color.FromRgb(200, 200, 200);
+                case "Magic":
+                    return Color.FromRgb(136, 136, 255);
+                case "Rare":
+                    return Color.FromRgb(255, 255, 119);
+                case "Unique":
+                    return Color.FromRgb(175, 96, 37);
+                default:
+                    //gem, currency, divination card, quest item, prophecy, relic
+                    return Color.FromRgb(170, 158, 130);
+            }
+        }
+
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            string rarity = value.ToString();
+            Color color = GetRarityColor(rarity);
+
+            return new SolidColorBrush(color);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
