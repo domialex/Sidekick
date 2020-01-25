308ac2cc07499cc6d55beb24c44e62458737277e
Zalhera <tobias.bechen@gmail.com>
2020-01-25 12:36:54 +0100

    Show Items Stats as Tooltip on Mouse Over

---
+++ src/Sidekick.Business/Trades/Results/ItemExtended.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Sidekick.Business.Trades.Results
+{
+    public class ItemExtended
+    {
+        public string Text { get; set; }
+    }
+}
