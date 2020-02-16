b8e28cd2e65274d7557c244cb0c876ef18c4f5fd
leMicin <lemire.yanic@gmail.com>
2020-02-15 20:34:28 -0500

    Color adjustments

--- src/Sidekick.UI/Leagues/RewardValueExtensions.cs
+++ src/Sidekick.UI/Leagues/RewardValueExtensions.cs
@@ -7,7 +7,7 @@ namespace Sidekick.UI.Leagues
             switch (value)
             {
                 case RewardValue.VeryHigh: return "#469408";
-                case RewardValue.High: return "#898989";
+                case RewardValue.High: return "#ffc107";
                 case RewardValue.Normal: return "#D9831F";
                 default: return "#D9230F";
             }
