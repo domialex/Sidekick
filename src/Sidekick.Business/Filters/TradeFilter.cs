7b9efc17fdd2f226cad16b4d03ba1fa107d352af
Stormbringer766 <storm7_662000@yahoo.com>
2020-02-15 18:25:47 -0600

    added trade filter with buyout

---
+++ src/Sidekick.Business/Filters/TradeFilter.cs
@@ -0,0 +1,8 @@
+﻿namespace Sidekick.Business.Filters
+{
+    public class TradeFilter
+    {
+        public bool Disabled { get; set; }
+        public TradeFilters Filters { get; set; } = new TradeFilters();
+    }
+}
