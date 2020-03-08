997e5ae7712dc5ae95b8bfe1d3e122b44ff1b016
leMicin <lemire.yanic@gmail.com>
2020-03-08 16:30:46 -0400

    Removed color on simple text values

--- src/Sidekick/Windows/Prices/ItemProperty.xaml.cs
+++ src/Sidekick/Windows/Prices/ItemProperty.xaml.cs
@@ -34,17 +34,15 @@ namespace Sidekick.Windows.Prices
             itemProperty.RichText.Document.Blocks.Clear();
             itemProperty.RichText.Document.Blocks.Add(new Paragraph(new Run(itemProperty.Property.Parsed)));
 
-            var matches = Highlight.Matches(itemProperty.Property.Parsed);
-
             // create textpointer translator
             var trans = new TextPointerTranslator(itemProperty.RichText.Document);
 
             // enumerate
-            for (var i = 0; i < matches.Count; i++)
+            foreach (var value in itemProperty.Property.Values)
             {
-                var info = matches[i];
-                var start = trans.GetTextPointer(info.Index, false);
-                var end = trans.GetTextPointer(info.Index + info.Value.Length, false);
+                var index = itemProperty.Property.Parsed.IndexOf(value.Value);
+                var start = trans.GetTextPointer(index, false);
+                var end = trans.GetTextPointer(index + value.Value.Length, false);
 
                 if (start == null || end == null)
                 {
@@ -52,13 +50,38 @@ namespace Sidekick.Windows.Prices
                 }
 
                 var range = new TextRange(start, end);
-
                 if (range != null)
                 {
                     range.ApplyPropertyValue(
-                       TextElement.ForegroundProperty, Brushes.LightGreen);
-                    range.ApplyPropertyValue(
                        TextElement.FontWeightProperty, FontWeight.FromOpenTypeWeight(700));
+
+                    switch (value.Type)
+                    {
+                        case PropertyType.Simple:
+                            range.ApplyPropertyValue(
+                               TextElement.ForegroundProperty, Brushes.White);
+                            break;
+                        case PropertyType.Augmented:
+                            range.ApplyPropertyValue(
+                               TextElement.ForegroundProperty, Brushes.LightGreen);
+                            break;
+                        case PropertyType.Cold:
+                            range.ApplyPropertyValue(
+                               TextElement.ForegroundProperty, Brushes.LightSkyBlue);
+                            break;
+                        case PropertyType.Fire:
+                            range.ApplyPropertyValue(
+                               TextElement.ForegroundProperty, Brushes.Red);
+                            break;
+                        case PropertyType.Lightning:
+                            range.ApplyPropertyValue(
+                               TextElement.ForegroundProperty, Brushes.LightYellow);
+                            break;
+                        case PropertyType.Chaos:
+                            range.ApplyPropertyValue(
+                               TextElement.ForegroundProperty, Brushes.Purple);
+                            break;
+                    }
                 }
             }
         }
