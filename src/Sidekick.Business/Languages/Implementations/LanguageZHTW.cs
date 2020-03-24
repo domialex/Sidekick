eb9099d37e69dfe28b452df4506e42d72a4a4b46
Arisix Chen <lopp543210102@gmail.com>
2020-03-24 11:40:38 +0800

    Update LanguageZHTW.cs

--- src/Sidekick.Business/Languages/Implementations/LanguageZHTW.cs
+++ src/Sidekick.Business/Languages/Implementations/LanguageZHTW.cs
@@ -10,6 +10,7 @@ namespace Sidekick.Business.Languages.Implementations
         public Uri PoeTradeApiBaseUrl => new Uri("http://web.poe.garena.tw/api/trade/");
         public Uri PoeCdnBaseUrl => new Uri("https://web.poecdn.com/");
         public Uri PoeWebsite => new Uri("http://web.poe.garena.tw/");
+
         public string RarityUnique => "傳奇";
         public string RarityRare => "稀有";
         public string RarityMagic => "魔法";
@@ -17,26 +18,17 @@ namespace Sidekick.Business.Languages.Implementations
         public string RarityCurrency => "通貨";
         public string RarityGem => "寶石";
         public string RarityDivinationCard => "命運卡";
-        public string DescriptionUnidentified => "未鑑定";
-        public string DescriptionQuality => "品質";
-        public string DescriptionCorrupted => "已汙染";
-        public string DescriptionRarity => "稀有度";
-        public string DescriptionSockets => "插槽";
-        public string DescriptionItemLevel => "物品等級";
-        public string DescriptionExperience => "經驗值";
-        public string DescriptionOrgan => "使用";
+
+        public string PrefixBlighted => "凋落的";
         public string PrefixSuperior => "精良的";
+
         public string InfluenceShaper => "塑者之物";
         public string InfluenceElder => "尊師之物";
         public string InfluenceCrusader => "聖戰軍王物品";
         public string InfluenceHunter => "狩獵者物品";
         public string InfluenceRedeemer => "救贖者物品";
         public string InfluenceWarlord => "總督軍物品";
-        public string DescriptionMapTier => "地圖階級";
-        public string DescriptionItemQuantity => "物品數量";
-        public string DescriptionItemRarity => "物品稀有度";
-        public string DescriptionMonsterPackSize => "怪物群大小";
-        public string PrefixBlighted => "凋落的";
+
         public string KeywordProphecy => "預言";
         public string KeywordVaal => "瓦爾";
         public string KeywordCatalyst => "催化劑";
@@ -48,21 +40,26 @@ namespace Sidekick.Business.Languages.Implementations
         public string KeywordVial => "之罈";
         public string KeywordEssence => "精髓";
 
-        public string DescriptionPhysicalDamage => "__TranslationRequired__";
-
-        public string DescriptionElementalDamage => "__TranslationRequired__";
-
-        public string DescriptionAttacksPerSecond => "__TranslationRequired__";
-
-        public string DescriptionCriticalStrikeChance => "__TranslationRequired__";
-
-        public string DescriptionEnergyShield => "__TranslationRequired__";
-
-        public string DescriptionArmour => "__TranslationRequired__";
-
-        public string DescriptionEvasion => "__TranslationRequired__";
-        public string DescriptionChanceToBlock => "__TranslationRequired__";
-
-        public string DescriptionLevel => "__TranslationRequired__";
+        public string DescriptionUnidentified => "未鑑定";
+        public string DescriptionQuality => "品質";
+        public string DescriptionCorrupted => "已汙染";
+        public string DescriptionRarity => "稀有度";
+        public string DescriptionSockets => "插槽";
+        public string DescriptionItemLevel => "物品等級";
+        public string DescriptionExperience => "經驗值";
+        public string DescriptionOrgan => "使用";
+        public string DescriptionPhysicalDamage => "物理傷害";
+        public string DescriptionElementalDamage => "元素傷害";
+        public string DescriptionAttacksPerSecond => "每秒攻擊次數";
+        public string DescriptionCriticalStrikeChance => "暴擊率";
+        public string DescriptionEnergyShield => "能量護盾";
+        public string DescriptionArmour => "護甲";
+        public string DescriptionEvasion => "閃避值";
+        public string DescriptionChanceToBlock => "格擋率";
+        public string DescriptionLevel => "物品等級";
+        public string DescriptionMapTier => "地圖階級";
+        public string DescriptionItemQuantity => "物品數量";
+        public string DescriptionItemRarity => "物品稀有度";
+        public string DescriptionMonsterPackSize => "怪物群大小";
     }
 }
