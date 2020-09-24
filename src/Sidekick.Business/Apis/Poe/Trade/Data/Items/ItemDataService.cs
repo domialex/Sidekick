4a14b53f1c25c8e29246e5fbbae2ba0e63af98a9
RuriRyan <ryan@btsoft.eu>
2020-09-24 23:32:46 +0200

    Remove unused using directives

--- src/Sidekick.Business/Apis/Poe/Trade/Data/Items/ItemDataService.cs
+++ src/Sidekick.Business/Apis/Poe/Trade/Data/Items/ItemDataService.cs
@@ -1,7 +1,5 @@
-using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using Sidekick.Business.Apis.Poe.Models;
