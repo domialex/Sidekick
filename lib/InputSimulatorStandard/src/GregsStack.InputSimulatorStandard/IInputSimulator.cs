394bd3e4127afa80b3e9f1df7bf50cbfc934efc8
leMicin <lemire.yanic@gmail.com>
2021-01-22 23:57:36 -0500

    Added SendInput library

---
+++ lib/InputSimulatorStandard/src/GregsStack.InputSimulatorStandard/IInputSimulator.cs
@@ -0,0 +1,26 @@
+﻿namespace GregsStack.InputSimulatorStandard
+{
+    /// <summary>
+    /// The contract for a service that simulates Keyboard and Mouse input and Hardware Input Device state detection for the Windows Platform.
+    /// </summary>
+    public interface IInputSimulator
+    {
+        /// <summary>
+        /// Gets the <see cref="IKeyboardSimulator"/> instance for simulating Keyboard input.
+        /// </summary>
+        /// <value>The <see cref="IKeyboardSimulator"/> instance.</value>
+        IKeyboardSimulator Keyboard { get; }
+
+        /// <summary>
+        /// Gets the <see cref="IMouseSimulator"/> instance for simulating Mouse input.
+        /// </summary>
+        /// <value>The <see cref="IMouseSimulator"/> instance.</value>
+        IMouseSimulator Mouse { get; }
+
+        /// <summary>
+        /// Gets the <see cref="IInputDeviceStateAdapter"/> instance for determining the state of the various input devices.
+        /// </summary>
+        /// <value>The <see cref="IInputDeviceStateAdapter"/> instance.</value>
+        IInputDeviceStateAdapter InputDeviceState { get; }
+    }
+}
---
+++ lib/InputSimulatorStandard/tests/GregsStack.InputSimulatorStandard.Tests/InputSimulatorTests.cs
@@ -0,0 +1,78 @@
+﻿namespace GregsStack.InputSimulatorStandard.Tests
+{
+    using System;
+
+    using Moq;
+
+    using Xunit;
+
+    public class InputSimulatorTests
+    {
+        private IInputSimulator inputSimulator;
+
+        public InputSimulatorTests()
+        {
+            this.inputSimulator = new InputSimulator(Mock.Of<IKeyboardSimulator>(), Mock.Of<IMouseSimulator>(), Mock.Of<IInputDeviceStateAdapter>());
+        }
+
+        public class Constructor : InputSimulatorTests
+        {
+            [Fact]
+            public void NullKeyboardSimulatorThrowsArgumentNullException()
+            {
+                Assert.Throws<ArgumentNullException>("keyboardSimulator", () =>
+                {
+                    this.inputSimulator = new InputSimulator(null, Mock.Of<IMouseSimulator>(), Mock.Of<IInputDeviceStateAdapter>());
+                });
+            }
+
+            [Fact]
+            public void NullMouseSimulatorThrowsArgumentNullException()
+            {
+                Assert.Throws<ArgumentNullException>("mouseSimulator", () =>
+                {
+                    this.inputSimulator = new InputSimulator(Mock.Of<IKeyboardSimulator>(), null, Mock.Of<IInputDeviceStateAdapter>());
+                });
+            }
+
+            [Fact]
+            public void NullInputDeviceStateAdapterThrowsArgumentNullException()
+            {
+                Assert.Throws<ArgumentNullException>("inputDeviceStateAdapter", () =>
+                {
+                    this.inputSimulator = new InputSimulator(Mock.Of<IKeyboardSimulator>(), Mock.Of<IMouseSimulator>(), null);
+                });
+            }
+        }
+
+        public class KeyboardProperty : InputSimulatorTests
+        {
+            [Fact]
+            public void IsNotNull()
+            {
+                var result = this.inputSimulator.Keyboard;
+                Assert.IsAssignableFrom<IKeyboardSimulator>(result);
+            }
+        }
+
+        public class MouseProperty : InputSimulatorTests
+        {
+            [Fact]
+            public void IsNotNull()
+            {
+                var result = this.inputSimulator.Mouse;
+                Assert.IsAssignableFrom<IMouseSimulator>(result);
+            }
+        }
+
+        public class InputDeviceStateProperty : InputSimulatorTests
+        {
+            [Fact]
+            public void IsNotNull()
+            {
+                var result = this.inputSimulator.InputDeviceState;
+                Assert.IsAssignableFrom<IInputDeviceStateAdapter>(result);
+            }
+        }
+    }
+}
