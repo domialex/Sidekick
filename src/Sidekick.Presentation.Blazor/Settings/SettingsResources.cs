166b37ad3c2557bcf3a072eee802fa16d4702f68
leMicin <lemire.yanic@gmail.com>
2021-01-30 02:36:30 -0500

    Started to create the settings layout

---
+++ src/Sidekick.Presentation.Blazor/Settings/SettingsResources.cs
@@ -0,0 +1,52 @@
+namespace Sidekick.Presentation.Blazor.Settings
+{
+    public class SettingsResources
+    {
+        protected SettingsResources() { }
+
+        public const string Cancel = "Cancel";
+        public const string Character_League = "Character_League";
+        public const string Character_Name = "Character_Name";
+        public const string Character_Title = "Character_Title";
+        public const string Chat = "Chat";
+        public const string Chat_Key_Exit = "Chat_Key_Exit";
+        public const string Chat_Key_Hideout = "Chat_Key_Hideout";
+        public const string Chat_Key_LeaveParty = "Chat_Key_LeaveParty";
+        public const string Chat_Key_ReplyToLastWhisper = "Chat_Key_ReplyToLastWhisper";
+        public const string Cheatsheets = "Cheatsheets";
+        public const string Cheatsheets_Key_Open = "Cheatsheets_Key_Open";
+        public const string General = "General";
+        public const string General_RetainClipboard = "General_RetainClipboard";
+        public const string General_ShowSplashScreen = "General_ShowSplashScreen";
+        public const string Group_Custom_Keybinds = "Group_Custom_Keybinds";
+        public const string Group_Keybinds = "Group_Keybinds";
+        public const string Group_Other = "Group_Other";
+        public const string Key_FindItems = "Key_FindItems";
+        public const string Key_OpenSettings = "Key_OpenSettings";
+        public const string Language_Parser = "Language_Parser";
+        public const string Language_Title = "Language_Title";
+        public const string Language_UI = "Language_UI";
+        public const string Map = "Map";
+        public const string Map_CloseWithMouse = "Map_CloseWithMouse";
+        public const string Map_Dangerous = "Map_Dangerous";
+        public const string Map_Dangerous_Regex = "Map_Dangerous_Regex";
+        public const string Map_Key_Check = "Map_Key_Check";
+        public const string Map_Key_Close = "Map_Key_Close";
+        public const string Ok = "Ok";
+        public const string Price = "Price";
+        public const string Price_CloseWithMouse = "Price_CloseWithMouse";
+        public const string Price_Key_Check = "Price_Key_Check";
+        public const string Price_Key_Close = "Price_Key_Close";
+        public const string Price_Key_OpenSearch = "Price_Key_OpenSearch";
+        public const string Price_Prediction_Enable = "Price_Prediction_Enable";
+        public const string ResetCache = "ResetCache";
+        public const string Stash = "Stash";
+        public const string Stash_EnableCtrlScroll = "Stash_EnableCtrlScroll";
+        public const string Stash_Key_Left = "Stash_Key_Left";
+        public const string Stash_Key_Right = "Stash_Key_Right";
+        public const string Title = "Title";
+        public const string Wiki = "Wiki";
+        public const string Wiki_Key_Open = "Wiki_Key_Open";
+        public const string Wiki_Preferred = "Wiki_Preferred";
+    }
+}
