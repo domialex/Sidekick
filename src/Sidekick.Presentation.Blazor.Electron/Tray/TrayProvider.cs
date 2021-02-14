017468dc07c167644d803121ffd4caca51eadaea
Dominique Alexandre <d.alexandre@outlook.com>
2021-02-14 17:10:03 -0500

    Fix env variable for electron, add development menu to tray icon

--- src/Sidekick.Presentation.Blazor.Electron/Tray/TrayProvider.cs
+++ src/Sidekick.Presentation.Blazor.Electron/Tray/TrayProvider.cs
@@ -1,68 +1,125 @@
 using System;
-using System.Collections.Generic;
 using ElectronNET.API.Entities;
+using MediatR;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Logging;
+using Sidekick.Domain.Game.Items.Commands;
+using Sidekick.Domain.Game.Trade.Commands;
+using Sidekick.Domain.Platforms;
 using Sidekick.Domain.Views;
 using Sidekick.Presentation.Localization.Tray;
+using Microsoft.Extensions.Hosting;
+using System.Linq;
+using System.Collections.Generic;
 
 namespace Sidekick.Presentation.Blazor.Electron.Tray
 {
     public class TrayProvider
     {
-        private readonly IWebHostEnvironment webHostEnvironment;
-        private readonly ILogger<TrayProvider> logger;
-        private readonly IViewLocator viewLocator;
+        private IWebHostEnvironment webHostEnvironment { get; init; }
+        private ILogger<TrayProvider> logger { get; init; }
+        private IViewLocator viewLocator { get; init; }
+        private IClipboardProvider clipboardProvider { get; init; }
+        private IMediator mediator { get; init; }
 
-        public TrayProvider(
-            IWebHostEnvironment webHostEnvironment,
-            ILogger<TrayProvider> logger,
-            IViewLocator viewLocator
-        )
+        public TrayProvider(IWebHostEnvironment webHostEnvironment,
+                            ILogger<TrayProvider> logger,
+                            IViewLocator viewLocator,
+                            IClipboardProvider clipboardProvider,
+                            IMediator mediator)
         {
             this.webHostEnvironment = webHostEnvironment;
             this.logger = logger;
             this.viewLocator = viewLocator;
+            this.clipboardProvider = clipboardProvider;
+            this.mediator = mediator;
         }
 
         public void Initialize()
         {
             try
             {
-                var menuItems = new List<MenuItem>
+                var menuItems = new List<MenuItem>()
                 {
-                    new MenuItem
+                    new ()
                     {
                         Label = "Cheatsheets",
                         Click = () => { viewLocator.Open(View.League); }
                     },
 
-                    new MenuItem
+                    new ()
                     {
                         Label = "About",
                         Click = () => { viewLocator.Open(View.About); }
                     },
 
-                    new MenuItem
+                    new ()
                     {
                         Label = "Settings",
                         Click = () => { viewLocator.Open(View.Settings); }
                     },
 
-                    new MenuItem
+                    new () { Type = MenuType.separator },
+
+                    new ()
                     {
                         Label = TrayResources.Exit,
                         Click = () => ElectronNET.API.Electron.App.Quit()
                     }
                 };
 
+
+                if (webHostEnvironment.IsDevelopment())
+                {
+                    menuItems.InsertRange(0, GetDevelopmentMenu());
+                }
+
                 ElectronNET.API.Electron.Tray.Show($"{webHostEnvironment.ContentRootPath}Assets/ExaltedOrb.png", menuItems.ToArray());
+                ElectronNET.API.Electron.Tray.OnDoubleClick += (_, _) => viewLocator.Open(View.Settings);
                 ElectronNET.API.Electron.Tray.SetToolTip(TrayResources.Title);
             }
             catch (Exception e)
             {
                 logger.LogError("Exception while initializing the tray.", e);
             }
+        }
+
+        public List<MenuItem> GetDevelopmentMenu()
+        {
+            return new List<MenuItem>()
+            {
+                 new () {
+                    Label = "Development",
+                    Type = MenuType.submenu,
+                    Submenu = new MenuItem[]
+                    {
+                        new () {
+                            Label = "Parse item from clipboard",
+                            Click = async () =>
+                            {
+                                var item = await mediator.Send(new ParseItemCommand(await clipboardProvider.GetText()));
+                                if (item != null)
+                                {
+                                    await mediator.Send(new PriceCheckItemCommand(item));
+                                }
+                            }
+                        },
+                        new () {
+                            Label = "Check map from clipboard",
+                            Click = async () =>
+                            {
+                                var item = await mediator.Send(new ParseItemCommand(await clipboardProvider.GetText()));
+                                if (item !=null && item.Properties.MapTier != 0)
+                                {
+                                    await viewLocator.Open(View.Map, item);
+                                }
+                            }
+                        }
+                    }
+                },
+
+                new () { Type = MenuType.separator },
+            };
         }
     }
 }
