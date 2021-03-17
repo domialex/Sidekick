42681ed6bdc53334afa9ba5c5985580bddb4f616
leMicin <lemire.yanic@gmail.com>
2021-03-17 19:41:59 -0400

    Updated packages

--- src/Sidekick.Presentation.Blazor/Startup.cs
+++ src/Sidekick.Presentation.Blazor/Startup.cs
@@ -86,8 +86,6 @@ namespace Sidekick.Presentation.Blazor
             }
             else
             {
-                app.UseExceptionHandler("/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
